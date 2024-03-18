50b0cd74c68c9f68648922a09f449f849d27e38e
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-18 11:50:45 +0530

    Added notiifcation cron

--- src/utils/common/cron-job.js
+++ src/utils/common/cron-job.js
@@ -1,17 +1,25 @@
 const cron = require('node-cron');
 
 const { EmailService } = require('../../services');
+const { MAILER } = require('../../config');
 const { SUCCESS } = require('./enum');
 
 function scheduleCrons(){
     cron.schedule('*/2 * * * *', async () => {
         const response = await EmailService.getPendingEmails();
         response.forEach((email) => {
-            EmailService.sendMail("notiairline4@gmail.com",
-                                    email.recipientEmail,
-                                    email.subject,
-                                    email.content
-                                    );
+            MAILER.sendMail({
+                to: email.recipientEmail,
+                subject: email.subject,
+                text: email.content
+            }, async (err, data) => {
+                if(err){
+                    console.log(err);
+                } else{
+                    console.log(data);
+                    await EmailService.updateTicket(email.id, {status: SUCCESS});
+                }
+            });
         })
 
         console.log(response);
